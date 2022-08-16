94363c0232e8fcf5f8337027160b1624638ed96c
hemanthsiripireddy <siripihemanth19@gmail.com>
2022-08-16 21:10:46 +0530

    ok

---
+++ DSAlgo_Java/src/general/MoveZeros.java
@@ -0,0 +1,17 @@
+package general;
+
+public class MoveZeros {
+	 public void moveZeroes(int[] nums) {
+	        int start=0;
+	        for(int i=0;i<nums.length;i++){
+	            if(nums[i]!=0){
+	                int temp=nums[start];
+	                nums[start]=nums[i];
+	                nums[i]=temp;
+	                start++;
+	            }
+	        }
+	        
+	    }
+
+}
